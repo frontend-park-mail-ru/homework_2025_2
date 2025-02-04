cedec5acef4c00065f7a39337de8e546f8111412
root <root@DESKTOP-QSQTRBJ.localdomain>
2025-02-04 23:45:28 +0300

    [variant-16]: начальный коммит

---
+++ test/fibonacciGenerator.js
@@ -0,0 +1,21 @@
+'use strict';
+
+QUnit.module("Тестируем функцию fibonacciGenerator", function() {
+    QUnit.test("Правильно генерирует первое число Фибоначчи", function(assert) {
+        const fibGen = fibonacciGenerator(1);
+
+        assert.deepEqual([...fibGen], [0], "Должно быть сгенерировано только первое число Фибоначчи.");
+    });
+    
+    QUnit.test("Правильно генерирует 5 первых чисел Фибоначчи", function(assert) {
+        const fibGen = fibonacciGenerator(5);
+
+        assert.deepEqual([...fibGen], [0, 1, 1, 2, 3], "Должны быть сгенерированы первые 5 чисел Фибоначчи.");
+    });
+
+    QUnit.test("Работает правильно с отрицательным числом чисел", function(assert) {
+        const fibGen = fibonacciGenerator(-5);
+
+        assert.deepEqual([...fibGen], [], "Генерация отрицательного числа должна вернуть пустой массив.");
+    });
+});
