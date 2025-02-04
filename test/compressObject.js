2d9b66092f254fe6c6a42f531eb27bb930090ab3
root <root@DESKTOP-QSQTRBJ.localdomain>
2025-02-04 23:45:40 +0300

    [variant-3]: начальный коммит

---
+++ test/compressObject.js
@@ -0,0 +1,31 @@
+'use strict';
+
+QUnit.module("Тестируем функцию compressObject", function() {
+    QUnit.test("Сжатие объекта с null, undefined и пустыми строками", function(assert) {
+        const result = compressObject({
+            name: "Андрей",
+            age: null,
+            city: "",
+            country: "Россия",
+            occupation: undefined
+        });
+
+        assert.deepEqual(result, { name: "Андрей", country: "Россия" }, "Должны остаться только ключи с ненулевыми значениями.");
+    });
+
+    QUnit.test("Работает с объектом без ненулевых значений", function(assert) {
+        const result = compressObject({
+            a: null,
+            b: undefined,
+            c: "",
+        });
+
+        assert.deepEqual(result, {}, "Объект без ненулевых значений должен вернуть пустой объект.");
+    });
+
+    QUnit.test("Работает с пустым объектом", function(assert) {
+        const result = compressObject({});
+
+        assert.deepEqual(result, {}, "Пустой объект должен вернуть пустой объект.");
+    });
+});
