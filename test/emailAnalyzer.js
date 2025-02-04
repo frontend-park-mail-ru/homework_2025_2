a3fafc634b5dd7dfb7648b886f463cf0ef7031cb
root <root@DESKTOP-QSQTRBJ.localdomain>
2025-02-04 23:45:21 +0300

    [variant-13]: начальный коммит

---
+++ test/emailAnalyzer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+QUnit.module("Тестируем функцию emailAnalyzer", function() {
+    QUnit.test("Работает правильно со строкой с одним email", function(assert) {
+        const input = "Мой email: user@example.com.";
+        const result = emailAnalyzer(input);
+
+        assert.deepEqual(result, {
+            emailCount: 1,
+            uniqueEmails: ["user@example.com"],
+            mostFrequentEmail: "user@example.com"
+        });
+    });
+
+    QUnit.test("Работает правильно со строкой с разными регистрами email", function(assert) {
+        const input = "Контакты: User@Example.com и user@example.com.";
+        const result = emailAnalyzer(input);
+
+        assert.deepEqual(result, {
+            emailCount: 2,
+            uniqueEmails: ["user@example.com"],
+            mostFrequentEmail: "user@example.com"
+        });
+    });
+
+    QUnit.test("Работает правильно со строкой с некорректными email", function(assert) {
+        const input = "Некорректные email: user@, @example.com, user@domain..com.";
+        const result = emailAnalyzer(input);
+
+        assert.deepEqual(result, {
+            emailCount: 0,
+            uniqueEmails: [],
+            mostFrequentEmail: ""
+        });
+    });
+});
+
+
