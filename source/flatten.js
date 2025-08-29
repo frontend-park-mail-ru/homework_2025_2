99852fc91fab624daf5c00c51126de506cbc2f6d
Robocotik <alexander.starkin@gmail.com>
2025-08-30 00:40:52 +0300

    [feat] source: add flatten function and documentation

---
+++ source/flatten.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Функция, принимающая на вход вложенный массив и возвращающая плоский массив
+ * @param {Array<any>} arr - массив любых элементов
+ *
+ * @example
+ * // returns [1, 2, 3]
+ * flatten([1, [2, [3]]]);
+ *
+ * @returns {Array<any>} Новый плоский массив
+ */
+
+const flatten = arr => {
+  const result = [];
+  arr.map(item => {
+    if (Array.isArray(item)) {
+      result.push(...flatten(item));
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
+};
