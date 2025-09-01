0b55b291512c5797ac30466798f102291da76242
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-09-01 12:06:07 +0300

    Реализация функции templateEngine, вариант 14, WEB-21 Григорьева Софья

---
+++ source/templateEngine.js
@@ -0,0 +1,38 @@
+/**
+ * Заменяет в шаблоне все вхождения переменных, заключённых в фигурные скобки, на соответствующие значения из объекта данных
+ * @param {string} templateString - Строка шаблона
+ * @param {Object} dataObject - Объект данных с значениями для подстановки
+ * 
+ * @example
+ * // returns "Привет, Технопарк!"
+ * templateEngine("Привет, {{name}}!", { name: "Технопарк" });
+ * 
+ * @returns {string} Результирующая строка с подставленными значениями
+ */
+function templateEngine(templateString, dataObject) {
+    return templateString.replace(/\{\{([^}]+)\}\}/g, (_, variableText) => {
+        return getVariableValue(variableText, dataObject);
+    });
+};
+
+/**
+ * Получает значение переменной из объекта по строке с именем
+ * @param {string} variableText - Строка с именем переменной
+ * @param {Object} dataObject - Объект данных, из которого извлекаются значения
+ *
+ * @example
+ * // returns "Москва"
+ * getVariableValue('address.city', { address: { city: "Москва", street: "2-я Бауманская" } });
+ * // returns ''
+ * getVariableValue('address.building', { address: { city: "Москва", street: "2-я Бауманская" } });
+ * 
+ * @returns {*} Значение переменной или пустая строка, если переменной не найдено
+ */
+function getVariableValue (variableText, dataObject) {
+    const parts = variableText.split('.');
+    let variableValue = dataObject;
+    for (let i = 0; i < parts.length; i++) {
+        variableValue = variableValue[parts[i]] ? variableValue[parts[i]] : '';
+    }
+    return variableValue;
+};
--- test/templateEngine.js
+++ test/templateEngine.js
@@ -24,5 +24,62 @@ QUnit.module("Тестируем функцию templateEngine", function() {
 
         assert.equal(result, "Город: Москва, Улица: 2-я Бауманская");
     });
+
+    QUnit.test("Работает правильно с пустым объектом", function(assert) {
+        const template = "Город: {{address.city}}, Улица: {{address.street}}";
+        const data = {};
+        const result = templateEngine(template, data);
+
+        assert.equal(result, "Город: , Улица: ");
+    });
+
+    QUnit.test("Работает правильно c несколькими одинаковыми переменными", function(assert) {
+        const template = "Привет, {{name}}! Hi, {{name}}!";
+        const data = { name: "Tехнопарк" };
+        const result = templateEngine(template, data);
+        
+        assert.equal(result, "Привет, Tехнопарк! Hi, Tехнопарк!");
+    });
+
+    QUnit.test("Работает правильно c null и undefined значениями", function(assert) {
+        const template = "Null: {{nullValue}}, Undefined: {{undefinedValue}}";
+        const data = { nullValue: null, undefinedValue: undefined };
+        const result = templateEngine(template, data);
+        
+        assert.equal(result, "Null: , Undefined: ");
+    });
+
+    QUnit.test("Работает правильно c пустой строкой как значением", function(assert) {
+        const template = "Имя: '{{name}}'";
+        const data = { name: "" };
+        const result = templateEngine(template, data);
+        
+        assert.equal(result, "Имя: ''");
+    });
+
+    QUnit.test("Работает правильно c глубокой вложенностью", function(assert) {
+        const template = "Значение: {{a.b.c.d.e.f}}";
+        const data = { a: { b: { c: { d: { e: { f: "значение" } } } } } };
+        const result = templateEngine(template, data);
+        
+        assert.equal(result, "Значение: значение");
+    });
+
+    QUnit.test("Работает правильно c шаблоном без переменных", function(assert) {
+        const template = "Простой текст без переменных";
+        const data = { name: "Технопарк" };
+        const result = templateEngine(template, data);
+        
+        assert.equal(result, "Простой текст без переменных");
+    });
+
+    QUnit.test("Работает правильно c неполными фигурными скобками", function(assert) {
+        const template = "{Текст} о {{name}}";
+        const data = { name: "Технопарке" };
+        const result = templateEngine(template, data);
+        
+        assert.equal(result, "{Текст} о Технопарке");
+    });
+
 });
 
