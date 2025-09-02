94990ef708b27c01223367a59583c1ae890887e9
Vladislav Lokhanev <88274622+VladislavLF@users.noreply.github.com>
2025-09-02 13:25:43 +0300

    Update flatten.js

--- source/flatten.js
+++ source/flatten.js
@@ -3,25 +3,22 @@
 /**
  * Функция, которая принимает вложенный массив (массив, содержащий другие массивы) 
  * и возвращает новый массив, в котором все элементы из вложенных массивов находятся на одном уровне.
- * @param {Array<Array<Numbers>>} input - вложенный массив чисел
- * @param {Array<Numbers>} result - результирующий массив
+ * @param {Array<Array>} input - вложенный массив чисел
  * 
  * @example
- * // returns [1, 2, 3, 4, 5]
- * flatten([1, 2, 3, 1, 2, [1, 1], [4, 4, 5, [2, 3]]]);
+ * // returns [1, {"a": 1}, 3, "abc", {"a": 1}, {"b": 1}, {"b": 1}, 1.23, {"c": 1}]
+ * flatten([1, {"a": 1}, 3, "abc", {"a": 1}, {"b": 1}, [{"b": 1}, [1.23, {"c": 1}]]]);
  * 
- * @returns {Array<Numbers>}
+ * @returns {Array}
  */
-const flatten = function(input, result=[]) {
-    input.forEach(function(item, i, arr) {
-        if (Array.isArray(item)) {
-            flatten(item, result);
+const flatten = function(input) {
+    let result = [];
+    for (let i = 0; i < input.length; i++) {
+        if (Array.isArray(input[i])) {
+            result = result.concat(flatten(input[i]));
         } else {
-            if (!result.includes(item)) {
-                result.push(item);
-            }
+            result.push(input[i]);
         }
-    });
+    }
     return result;
-
 };
