6676fad90a2071553424a3da1930fcb392405db0
andrey57x <57andreys57@gmail.com>
2025-09-02 18:52:12 +0300

    Fixed JSDoc

--- source/partition.js
+++ source/partition.js
@@ -1,19 +1,18 @@
 'use strict';
 
 /**
- * Разбивает массив на два подмассива по предикату.
- *
- * @template T
- * @param {Array<T>} arr Массив элементов для разделения.
- * @param {(value: T) => boolean} predicate
- *        Функция-предикат, принимающая элемент массива,
- *        должна возвращать логическое значение (`true`/`false`).
- * @returns {Array<Array<T>>} Двумерный массив: `[matching, nonMatching]`.
+ * Разбивает массив на два подмассива по предикату
+ * @param {Array<*>} arr Массив элементов для разделения
+ * @param {function(*): boolean} predicate
+ *        Функция-предикат, принимающая элемент массива и
+ *        возвращающая логическое значение (`true`/`false`)
  *
  * @example
  * // Разделить числа на чётные и нечётные
  * const [evens, odds] = partition([1,2,3,4], n => n % 2 === 0);
  * // evens => [2,4], odds => [1,3]
+ * 
+ * @returns {Array<Array<*>>} Двумерный массив: `[matching, nonMatching]`
  */
 function partition(arr, predicate) {
     const matching = [];
