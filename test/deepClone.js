6933b3fecd7e03eb78fdefed5e83913d04348be1
Stephen Markman <rebzinka@gmail.com>
2025-09-02 20:06:04 +0300

    HW Done v2

--- test/deepClone.js
+++ test/deepClone.js
@@ -24,4 +24,23 @@ QUnit.module('Тестируем функцию deepClone', () => {
         assert.deepEqual(cloned, original, 'Копия массива должна быть равна оригиналу');
         assert.notStrictEqual(cloned[2], original[2], 'Вложенный объект в массиве должен быть независимым');
     });
+
+    QUnit.test('Работает правильно для специальных констант', (assert) => {
+        assert.strictEqual(deepClone(Infinity), Infinity);
+        assert.strictEqual(deepClone(0), 0);
+        assert.strictEqual(deepClone(-Infinity), -Infinity);
+    });
+
+    QUnit.test('Работает правильно для функций', (assert) => {
+        assert.strictEqual(deepClone(() => {}), () => {});
+    });
+
+    QUnit.test('Работает правильно для символов', (assert) => {
+        assert.strictEqual(deepClone(Symbol()), Symbol());
+    });
+
+    QUnit.test('Работает правильно для булевых значений', (assert) => {
+        assert.strictEqual(deepClone(true), true);
+        assert.strictEqual(deepClone(false), false);
+    });
 });
