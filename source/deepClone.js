90188bedb48f1c4160986faa811b7c0a55ce7d08
Stephen Markman <rebzinka@gmail.com>
2025-09-02 20:13:45 +0300

    HW Done v3

--- source/deepClone.js
+++ source/deepClone.js
@@ -1,14 +1,45 @@
 'use strict';
 
 /**
- * Функция, возвращающая копию объекта
- * @param {Object} obj - объект
- * 
+ * Функция, создающая глубокую копию значения
+ * @param {*} obj - исходное значение (объект, массив, и тд)
+ *
  * @example
- * // returns { a: 1, b: 2 }
- * deepClone({ a: 1, b: 2 });
- * 
- * @returns {Object}
- * */
+ * // returns { a: 1, b: { c: 2 } }
+ * deepClone({ a: 1, b: { c: 2 } });
+ *
+ * @example
+ * // returns [1, [2, 3]]
+ * deepClone([1, [2, 3]]);
+ *
+ * @example
+ * // returns "hello"
+ * deepClone("hello");
+ *
+ * @returns {*} - глубокая копия исходного значения
+ */
+const deepClone = function(obj) {
+    if (obj === null || obj === undefined) {
+        return obj;
+    }
 
-const deepClone = obj => JSON.parse(JSON.stringify(obj));
+    if (typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (Array.isArray(obj)) {
+        return obj.map(item => deepClone(item));
+    }
+
+    if (obj instanceof Date) {
+        return new Date(obj.getTime());
+    }
+
+    const clonedObj = {};
+    for (let key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            clonedObj[key] = deepClone(obj[key]);
+        }
+    }
+    return clonedObj;
+};
--- test/deepClone.js
+++ test/deepClone.js
@@ -1,7 +1,7 @@
 'use strict';
 
 QUnit.module('Тестируем функцию deepClone', () => {
-    QUnit.test('Работает правильного для простого объекта', (assert) => {
+    QUnit.test('Работает правильно для простого объекта', (assert) => {
         const original = { a: 1, b: 2 };
         const cloned = deepClone(original);
 
@@ -14,6 +14,7 @@ QUnit.module('Тестируем функцию deepClone', () => {
         const cloned = deepClone(original);
 
         assert.deepEqual(cloned, original, 'Копия должна быть равна оригиналу');
+        assert.notStrictEqual(cloned, original, 'Копия должна быть независимой от оригинала');
         assert.notStrictEqual(cloned.b, original.b, 'Вложенный объект должен быть независимым');
     });
 
@@ -22,25 +23,62 @@ QUnit.module('Тестируем функцию deepClone', () => {
         const cloned = deepClone(original);
 
         assert.deepEqual(cloned, original, 'Копия массива должна быть равна оригиналу');
+        assert.notStrictEqual(cloned, original, 'Копия массива должна быть независимой от оригинала');
         assert.notStrictEqual(cloned[2], original[2], 'Вложенный объект в массиве должен быть независимым');
     });
 
-    QUnit.test('Работает правильно для специальных констант', (assert) => {
-        assert.strictEqual(deepClone(Infinity), Infinity);
-        assert.strictEqual(deepClone(0), 0);
-        assert.strictEqual(deepClone(-Infinity), -Infinity);
+    QUnit.test('Работает правильно для специальных числовых значений', (assert) => {
+        assert.strictEqual(deepClone(Infinity), Infinity, 'Должна корректно копировать Infinity');
+        assert.strictEqual(deepClone(-Infinity), -Infinity, 'Должна корректно копировать -Infinity');
+        assert.strictEqual(deepClone(0), 0, 'Должна корректно копировать 0');
+        assert.strictEqual(deepClone(-0), -0, 'Должна корректно копировать -0');
+        assert.ok(Number.isNaN(deepClone(NaN)), 'Должна корректно копировать NaN');
     });
 
-    QUnit.test('Работает правильно для функций', (assert) => {
-        assert.strictEqual(deepClone(() => {}), () => {});
+    QUnit.test('Работает правильно с null и undefined', (assert) => {
+        assert.strictEqual(deepClone(null), null, 'Должна корректно копировать null');
+        assert.strictEqual(deepClone(undefined), undefined, 'Должна корректно копировать undefined');
     });
 
-    QUnit.test('Работает правильно для символов', (assert) => {
-        assert.strictEqual(deepClone(Symbol()), Symbol());
+    QUnit.test('Работает правильно для примитивных значений', (assert) => {
+        assert.strictEqual(deepClone(42), 42, 'Должна корректно копировать числа');
+        assert.strictEqual(deepClone('hello'), 'hello', 'Должна корректно копировать строки');
+        assert.strictEqual(deepClone(true), true, 'Должна корректно копировать true');
+        assert.strictEqual(deepClone(false), false, 'Должна корректно копировать false');
     });
 
-    QUnit.test('Работает правильно для булевых значений', (assert) => {
-        assert.strictEqual(deepClone(true), true);
-        assert.strictEqual(deepClone(false), false);
+    QUnit.test('Работает правильно для объектов Date', (assert) => {
+        const date = new Date();
+        const cloned = deepClone(date);
+        
+        assert.deepEqual(cloned, date, 'Скопированная дата должна быть равна оригиналу');
+        assert.notStrictEqual(cloned, date, 'Скопированная дата должна быть новым объектом');
+        assert.ok(cloned instanceof Date, 'Скопированный объект должен быть экземпляром Date');
+    });
+
+    QUnit.test('Работает правильно для вложенных структур с разными типами', (assert) => {
+        const original = {
+            num: 42,
+            str: 'hello',
+            bool: true,
+            nil: null,
+            date: new Date(),
+            arr: [1, 2, { nested: 'value' }],
+            obj: {
+                inner: {
+                    deep: 'deep value'
+                }
+            }
+        };
+        
+        const cloned = deepClone(original);
+        
+        assert.deepEqual(cloned, original, 'Сложная структура должна копироваться корректно');
+        assert.notStrictEqual(cloned, original, 'Основной объект должен быть независимым');
+        assert.notStrictEqual(cloned.date, original.date, 'Date внутри должен быть независимым');
+        assert.notStrictEqual(cloned.arr, original.arr, 'Массив должен быть независимым');
+        assert.notStrictEqual(cloned.arr[2], original.arr[2], 'Объекты в массиве должны быть независимыми');
+        assert.notStrictEqual(cloned.obj, original.obj, 'Вложенные объекты должны быть независимыми');
+        assert.notStrictEqual(cloned.obj.inner, original.obj.inner, 'Глубоко вложенные объекты должны быть независимыми');
     });
 });
