2b1527006fac43aaf5cf3bcd3c929f3cd072c90a
misha <mr.loshkariov@yandex.ru>
2025-09-03 23:24:00 +0300

    добавлены новые тесты, переписан код, добавлен undefined при не строке, добавлена обработка new String

--- source/emailAnalyzer.js
+++ source/emailAnalyzer.js
@@ -1,7 +1,8 @@
 /**
  * Функция, определяет корректные email адреса в заданной стоке и
  * возвращает количеcтсво email, список уникальных email, самый частотный email
- * @param {string} emails - строка
+ * @param {string|String} rawString - строка
+ *
  *
  * @example
  * const result = analyzeEmails("test@mail.com, user@mail.com");
@@ -14,29 +15,32 @@
  *   mostFrequentEmail: string
  * }}
  */
-function emailAnalyzer(emails)
+
+function emailAnalyzer(rawString)
 {
-    const REGULAR_OF_EMAIL= /(?:[a-zA-Z0-9_%+-]+\.?)+[a-zA-Z0-9_%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}/g;
-    let normalEmails = emails.match(REGULAR_OF_EMAIL)?? [];
-    normalEmails = normalEmails.map((email) => email.toLowerCase())
-    let uniqueEmailsMap = new Map();
-    for ( let email of normalEmails)
+    if (typeof rawString!=="string" && !(rawString instanceof String))
     {
-        uniqueEmailsMap.set(email, (uniqueEmailsMap.get(email) ?? 0)+1);
+        return undefined
     }
+    const REGULAR_OF_EMAIL= /(?:[a-zA-Z0-9_%+-]+\.?)+[a-zA-Z0-9_%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}/g;
+    let emails = rawString.match(REGULAR_OF_EMAIL) ?? [];
+    let normalizedEmails = emails.map((email) => email.toLowerCase())
+
+    let uniqueEmailsMap = new Map();
+    normalizedEmails.forEach((email)=> uniqueEmailsMap.set(email, (uniqueEmailsMap.get(email) ?? 0)+1));
+
     let maxKey = null;
     let maxFreq = -Infinity;
-    for (const [key,freq] of uniqueEmailsMap)
-    {
-        if (freq> maxFreq){
+    uniqueEmailsMap.forEach((freq, key) => {
+        if (freq > maxFreq) {
             maxFreq = freq;
             maxKey = key;
         }
-    }
+    })
+
     return {
-        emailCount: normalEmails.length,
+        emailCount: normalizedEmails.length,
         uniqueEmails: [...uniqueEmailsMap.keys()],
         mostFrequentEmail: maxKey ?? ''
     }
-
 }
--- test/emailAnalyzer.js
+++ test/emailAnalyzer.js
@@ -66,6 +66,7 @@ QUnit.module("Тестируем функцию emailAnalyzer", function () {
             mostFrequentEmail: "user@gmail.com"
         });
     });
+
     QUnit.test("Работает правильно несколькими корректными email", function (assert) {
         const input = "мои email адреса: user@gmail.com, admin@admin.com, user@example.ru, profile@gmail.uu";
         const result = emailAnalyzer(input);
@@ -76,6 +77,7 @@ QUnit.module("Тестируем функцию emailAnalyzer", function () {
             mostFrequentEmail: "user@gmail.com"
         });
     });
+
     QUnit.test("Работает правильно c много повторяющимся email", function (assert) {
         const input = "мои email адреса: user@gmail.com, user@gmail.com, user@gmail.com, user@gmail.com, user@gmail.com, user@gmail.com";
         const result = emailAnalyzer(input);
@@ -87,6 +89,44 @@ QUnit.module("Тестируем функцию emailAnalyzer", function () {
         });
     });
 
+    QUnit.test("Работает правильно c русским языком", function (assert) {
+        const input = "мои email адреса: миша@hmail.ru, example@admin.com;"
+        const result = emailAnalyzer(input);
+
+        assert.deepEqual(result, {
+            emailCount: 1,
+            uniqueEmails: ["example@admin.com"],
+            mostFrequentEmail: "example@admin.com"
+        });
+    });
+
+    QUnit.test("Работает правильно cо спец символами", function (assert) {
+        const input = "мои email адреса: №;%:?;№@hmail.ru, exa++m--p%le@admin.com; e%xa+m-&ple@admin.com"
+        const result = emailAnalyzer(input);
+
+        assert.deepEqual(result, {
+            emailCount: 2,
+            uniqueEmails: ["exa++m--p%le@admin.com", "ple@admin.com"],
+            mostFrequentEmail: "exa++m--p%le@admin.com"
+        });
+    });
+
+    QUnit.test("Работает правильно c не строкой", function (assert) {
+        const input = [1,2,3]
+        const result = emailAnalyzer(input);
+
+        assert.deepEqual(result, undefined);
+    });
+
+    QUnit.test("Работает правильно c new String", function (assert) {
+        const input = new String("Мой email: user@example.com.");
+        const result = emailAnalyzer(input);
+
+        assert.deepEqual(result, {
+            emailCount: 1,
+            uniqueEmails: ["user@example.com"],
+            mostFrequentEmail: "user@example.com"
+        });
+    });
+
 });
-
-
