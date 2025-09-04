25f4a0114332cf66618029d9ce60fd1b6b902195
kenot_1312 <dimma.dort@gmail.com>
2025-09-04 17:39:28 +0300

    Added findUniqueProperties function

---
+++ source/findUniqueProperties.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Функция, определяющая максимальное число в массиве
+ * @param {Array<Number>} numbers - массив чисел
+ *
+ * @example
+ * // returns 3
+ * max([1, 2, 3]);
+ *
+ * @returns {Number}
+ */
+
