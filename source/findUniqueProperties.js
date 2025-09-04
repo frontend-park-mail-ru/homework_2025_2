db8274ae5a3b9d23c41c85d637c0230d32062fc4
gettingtghingsdoneee <wrjmll@gmail.com>
2025-09-04 19:45:02 +0000

    Исправлены проблемы с качеством кода

--- source/findUniqueProperties.js
+++ source/findUniqueProperties.js
@@ -2,32 +2,36 @@
  
 /**
  * Функция, возвращающая объект с уникальными свойствами из произвольного числа объектов
- * @param {...Object} objects - объекты
+ * @param {...Object} objects - передаваемые объекты
  * 
  * @example
  * // returns { a: 1, d: 5, e: 6 }
- *  findUniqueProperties(
+ * findUniqueProperties(
  *  { a: 1, b: 2, c: 3 },
  *  { b: 2, c: 4, d: 5 },
  *  { b: 2, e: 6 }
- *  );
+ * );
  * 
- * @returns {Object} 
+ * @returns {Object} - объект с уникальными properties
  */
-
 const findUniqueProperties = (...objects) => {
     const result = {};
+    const keyCounts = {};
     
     objects.forEach(obj => {
-        for (let key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                if (result[key] == null) { 
-                    result[key] = obj[key];
-                } else {
-                    delete result[key];
-                } 
+        Object.keys(obj).forEach(key => {
+            if (keyCounts[key] === undefined) {
+                keyCounts[key] = 0;
             }
-        }
+
+            keyCounts[key] += 1;
+
+            if (keyCounts[key] === 1) { 
+                result[key] = obj[key];
+            } else if (keyCounts[key] === 2) {
+                delete result[key];
+            } 
+        })
     });
     
     return result;
