9bd05e6b9386c13edac45c6646d49a27768f457b
andrey57x <57andreys57@gmail.com>
2025-09-06 16:36:40 +0300

    Fixed JSDoc, function and TypeGuard

--- source/partition.js
+++ source/partition.js
@@ -2,7 +2,7 @@
 
 /**
  * Разбивает массив на два подмассива по предикату
- * @param {Array<*>} arr Массив элементов для разделения
+ * @param {Array} arr Массив элементов для разделения
  * @param {function(*): boolean} predicate
  *        Функция-предикат, принимающая элемент массива и
  *        возвращающая логическое значение (`true`/`false`)
@@ -12,17 +12,19 @@
  * const [evens, odds] = partition([1,2,3,4], n => n % 2 === 0);
  * // evens => [2,4], odds => [1,3]
  * 
- * @returns {Array<Array<*>>} Двумерный массив: `[matching, nonMatching]`
+ * @returns Двумерный массив из подходящих и не подходящих значений.
  */
-function partition(arr, predicate) {
-    const matching = [];
-    const nonMatching = [];
-    for (let elem of arr) {
-        if (predicate(elem)) {
-            matching.push(elem);
-        } else {
-            nonMatching.push(elem);
-        }
-    }
-    return [matching, nonMatching];
+const partition = (arr, predicate) => {
+    if (!Array.isArray(arr)) throw new TypeError('Expected first argument to be an Array');
+    if (typeof predicate !== 'function') throw new TypeError('Expected second argument to be a Function');
+    return arr.reduce(
+        (acc, item) => {
+            if (item === null) throw new TypeError('Null elements not allowed');
+            if (typeof item === 'symbol') throw new TypeError('Symbol elements not allowed');
+            if (typeof item === 'number' && Number.isNaN(item)) throw new TypeError('NaN not allowed');
+            (predicate(item) ? acc[0] : acc[1]).push(item);
+            return acc;
+        },
+        [[], []]
+    );
 }
